b0c99ffa31000416e40d49786f47626e48f52aef
dinbecarevic <din.velenje@gmail.com>
2023-02-20 13:56:31 +0100

    Initial commit (login/register)

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { :registrations => "registrations" }
+  root to: "pages#home"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get 'dashboard/index'
+end
